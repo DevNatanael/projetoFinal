397d5d2974de70404aefcbde23254312d560d41f
natanael <natanael.guedes@tijucaalimentos.com>
2023-11-16 10:55:13 -0300

    adicionando dark mode tela produtos

---
+++ script/produtos.js
@@ -0,0 +1,30 @@
+const toggleDark = document.getElementById('toggle-dark-products')
+const html = document.querySelector('html')
+const sol = document.getElementById('sol')
+const lua = document.getElementById('lua')
+
+let darkMode = false;
+
+mudaIcone();
+
+toggleDark.addEventListener('click',function(){
+    if(darkMode){
+        html.classList.remove('dark-mode')
+        darkMode = false
+    }else{
+        html.classList.add('dark-mode')
+        darkMode = true
+    }
+
+    mudaIcone();
+})
+
+function mudaIcone(){
+    if(darkMode){
+        sol.style.opacity = '0'
+        lua.style.opacity = '1'
+    }else{
+        sol.style.opacity = '1'
+        lua.style.opacity = '0'
+    }
+}
